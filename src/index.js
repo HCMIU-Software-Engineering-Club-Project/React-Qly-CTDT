d817bc0e5cc608e2dbd41a546081cb540d2b2f5e
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2019-09-24 11:15:11 +0700

    Static HTML & CSS

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute } from "react-router";
+import { HashRouter } from "react-router-dom";
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+//import Layout from './components/Layout';
+
+/*ReactDOM.render(
+    <HashRouter>
+        <Route path="/" component={App}>
+        </Route>
+    </HashRouter>, 
+    document.getElementById('root'));*/
+
+serviceWorker.unregister();
